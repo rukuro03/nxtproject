41546d9cfa65a958e82281e1ee7947bc75a50c29
rukuro03 <radonedoes@gmail.com>
2018-12-06 17:29:33 +0900

    red boll

--- monoatume.c
+++ monoatume.c
@@ -39,6 +39,8 @@ void func_calib(){
   CalibArm();
 }
 
+
+
 void strategy(){
   FLGPTN flg;
 
@@ -46,19 +48,40 @@ void strategy(){
   //しばらくまっすぐすすんで
   act_tsk(Ttimer);
   act_tsk(Tmusc);
-  LogString("Start");
+
+  //サンプル
+  //緑出てはいるだけ
+  /*LogString("Start");
   flg = MoveLength(40,0,100);
   LogString("Turn right");
   flg = MoveLength(50, 200, 100);
   LogString("Turn left");
   flg = MoveLength(50,-200,100);
-  //前後にぷるぷる
   LogString("Go back");
-  flg = MoveLength(-20,0,100);
+  flg = MoveLength(70,0,250);
   LogString("and forth");
-  flg = MoveLength(20,0,100);
+  flg = MoveLength(-70,0,250);
   //アームを上げる
+  MoveArm(ARM_UP);*/
+
+  //ちゃんと集めるやつ
+  //赤ボールを取る
+  flg = MoveLength(70, 0, 900);
+  //バックする
+  flg = MoveLength(-70, 40, 300);
+  //タイヤを取りに行く
+  flg = MoveLength(50, 0, 400);
+  //タイヤの前で 3秒停止　アームを上げる　ゆっくり前に進む
+  dly_tsk(3000);
   MoveArm(ARM_UP);
+  flg = MoveLength(20, 0, 100);
+  //方向転換
+  MoveArm(ARM_DOWN);
+  flg = MoveLength(70, -200, 80);
+  //スタート位置に戻る
+  flg = MoveLength(70, 0, 500);
+ 
+  
 }
 
 void func_menu(){
